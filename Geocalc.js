f81a6f1a387bf52921e303cf56a36ff7f0f2244e
lvdq <lvdq@mail.taiji.com.cn>
2018-12-06 16:48:54 +0800

    计算两个经纬度点的弧度（和正东方向的逆时针夹角）

--- Geocalc.js
+++ Geocalc.js
@@ -1,2 +1,15 @@
-const Geocalc ={
+const Geocalc = {
+    toRadian:function(dAngle){
+        return dAngle * Math.PI /180;
+    },
+    toAngle:function(dRadian){
+        return dRadian * 180 / Math.PI;
+    },
+    getPntRadian:function (pntStart, pntEnd) {
+		//计算两个经纬度点的弧度（和正东方向的逆时针夹角）。
+		var i, o, n = 0;
+		i = pntEnd.lng - pntStart.lng, o = pntEnd.lat - pntStart.lat;
+        return   (n = Math.atan2(o, i)) < 0 && (n += 2 * Math.PI), n
+    }
 }
+export default Geocalc
